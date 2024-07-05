2660e75b486919685d35f16e4652bea1a64bee3d
Berika Bal <berikaaball@gmail.com>
2024-07-06 01:51:24 +0300

    SayısalLoto added

---
+++ SayısalLoto/SayısalLoto/Form1.Designer.vb
@@ -0,0 +1,346 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuperStarCBox = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SStarLbl = New System.Windows.Forms.Label()
+        Me.Sayi6Lbl = New System.Windows.Forms.Label()
+        Me.Sayi5Lbl = New System.Windows.Forms.Label()
+        Me.Sayi4Lbl = New System.Windows.Forms.Label()
+        Me.Sayi3Lbl = New System.Windows.Forms.Label()
+        Me.Sayi2Lbl = New System.Windows.Forms.Label()
+        Me.OynaBtn = New System.Windows.Forms.Button()
+        Me.Sayi1Lbl = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'SuperStarCBox
+        '
+        Me.SuperStarCBox.AutoSize = True
+        Me.SuperStarCBox.Location = New System.Drawing.Point(57, 180)
+        Me.SuperStarCBox.Margin = New System.Windows.Forms.Padding(4)
+        Me.SuperStarCBox.Name = "SuperStarCBox"
+        Me.SuperStarCBox.Size = New System.Drawing.Size(127, 20)
+        Me.SuperStarCBox.TabIndex = 19
+        Me.SuperStarCBox.Text = "Süper Star Oyna"
+        Me.SuperStarCBox.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label2.Location = New System.Drawing.Point(505, 48)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 39)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "+"
+        '
+        'SStarLbl
+        '
+        Me.SStarLbl.BackColor = System.Drawing.Color.SaddleBrown
+        Me.SStarLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.SStarLbl.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.SStarLbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.SStarLbl.ForeColor = System.Drawing.Color.White
+        Me.SStarLbl.Location = New System.Drawing.Point(554, 36)
+        Me.SStarLbl.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.SStarLbl.Name = "SStarLbl"
+        Me.SStarLbl.Size = New System.Drawing.Size(66, 61)
+        Me.SStarLbl.TabIndex = 17
+        Me.SStarLbl.Tag = ""
+        Me.SStarLbl.Text = "00"
+        Me.SStarLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Sayi6Lbl
+        '
+        Me.Sayi6Lbl.BackColor = System.Drawing.Color.LimeGreen
+        Me.Sayi6Lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Sayi6Lbl.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Sayi6Lbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Sayi6Lbl.ForeColor = System.Drawing.Color.White
+        Me.Sayi6Lbl.Location = New System.Drawing.Point(430, 36)
+        Me.Sayi6Lbl.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Sayi6Lbl.Name = "Sayi6Lbl"
+        Me.Sayi6Lbl.Size = New System.Drawing.Size(66, 61)
+        Me.Sayi6Lbl.TabIndex = 16
+        Me.Sayi6Lbl.Tag = "5"
+        Me.Sayi6Lbl.Text = "00"
+        Me.Sayi6Lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Sayi5Lbl
+        '
+        Me.Sayi5Lbl.BackColor = System.Drawing.Color.LimeGreen
+        Me.Sayi5Lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Sayi5Lbl.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Sayi5Lbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Sayi5Lbl.ForeColor = System.Drawing.Color.White
+        Me.Sayi5Lbl.Location = New System.Drawing.Point(356, 36)
+        Me.Sayi5Lbl.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Sayi5Lbl.Name = "Sayi5Lbl"
+        Me.Sayi5Lbl.Size = New System.Drawing.Size(66, 61)
+        Me.Sayi5Lbl.TabIndex = 15
+        Me.Sayi5Lbl.Tag = "4"
+        Me.Sayi5Lbl.Text = "00"
+        Me.Sayi5Lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Sayi4Lbl
+        '
+        Me.Sayi4Lbl.BackColor = System.Drawing.Color.LimeGreen
+        Me.Sayi4Lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Sayi4Lbl.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Sayi4Lbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Sayi4Lbl.ForeColor = System.Drawing.Color.White
+        Me.Sayi4Lbl.Location = New System.Drawing.Point(281, 36)
+        Me.Sayi4Lbl.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Sayi4Lbl.Name = "Sayi4Lbl"
+        Me.Sayi4Lbl.Size = New System.Drawing.Size(66, 61)
+        Me.Sayi4Lbl.TabIndex = 14
+        Me.Sayi4Lbl.Tag = "3"
+        Me.Sayi4Lbl.Text = "00"
+        Me.Sayi4Lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Sayi3Lbl
+        '
+        Me.Sayi3Lbl.BackColor = System.Drawing.Color.LimeGreen
+        Me.Sayi3Lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Sayi3Lbl.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Sayi3Lbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Sayi3Lbl.ForeColor = System.Drawing.Color.White
+        Me.Sayi3Lbl.Location = New System.Drawing.Point(206, 36)
+        Me.Sayi3Lbl.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Sayi3Lbl.Name = "Sayi3Lbl"
+        Me.Sayi3Lbl.Size = New System.Drawing.Size(66, 61)
+        Me.Sayi3Lbl.TabIndex = 13
+        Me.Sayi3Lbl.Tag = "2"
+        Me.Sayi3Lbl.Text = "00"
+        Me.Sayi3Lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Sayi2Lbl
+        '
+        Me.Sayi2Lbl.BackColor = System.Drawing.Color.LimeGreen
+        Me.Sayi2Lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Sayi2Lbl.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Sayi2Lbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Sayi2Lbl.ForeColor = System.Drawing.Color.White
+        Me.Sayi2Lbl.Location = New System.Drawing.Point(132, 36)
+        Me.Sayi2Lbl.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Sayi2Lbl.Name = "Sayi2Lbl"
+        Me.Sayi2Lbl.Size = New System.Drawing.Size(66, 61)
+        Me.Sayi2Lbl.TabIndex = 12
+        Me.Sayi2Lbl.Tag = "1"
+        Me.Sayi2Lbl.Text = "00"
+        Me.Sayi2Lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'OynaBtn
+        '
+        Me.OynaBtn.Location = New System.Drawing.Point(57, 114)
+        Me.OynaBtn.Margin = New System.Windows.Forms.Padding(4)
+        Me.OynaBtn.Name = "OynaBtn"
+        Me.OynaBtn.Size = New System.Drawing.Size(564, 44)
+        Me.OynaBtn.TabIndex = 11
+        Me.OynaBtn.Text = "Oyna"
+        Me.OynaBtn.UseVisualStyleBackColor = True
+        '
+        'Sayi1Lbl
+        '
+        Me.Sayi1Lbl.BackColor = System.Drawing.Color.LimeGreen
+        Me.Sayi1Lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Sayi1Lbl.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Sayi1Lbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Sayi1Lbl.ForeColor = System.Drawing.Color.White
+        Me.Sayi1Lbl.Location = New System.Drawing.Point(57, 36)
+        Me.Sayi1Lbl.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Sayi1Lbl.Name = "Sayi1Lbl"
+        Me.Sayi1Lbl.Size = New System.Drawing.Size(66, 61)
+        Me.Sayi1Lbl.TabIndex = 10
+        Me.Sayi1Lbl.Tag = "0"
+        Me.Sayi1Lbl.Text = "00"
+        Me.Sayi1Lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Red
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(477, 261)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 61)
+        Me.Label4.TabIndex = 25
+        Me.Label4.Tag = "11"
+        Me.Label4.Text = "00"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.Red
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(403, 261)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(66, 61)
+        Me.Label5.TabIndex = 24
+        Me.Label5.Tag = "10"
+        Me.Label5.Text = "00"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.Red
+        Me.Label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(328, 261)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(66, 61)
+        Me.Label6.TabIndex = 23
+        Me.Label6.Tag = "9"
+        Me.Label6.Text = "00"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.Red
+        Me.Label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(253, 261)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(66, 61)
+        Me.Label7.TabIndex = 22
+        Me.Label7.Tag = "8"
+        Me.Label7.Text = "00"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label8
+        '
+        Me.Label8.BackColor = System.Drawing.Color.Red
+        Me.Label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.White
+        Me.Label8.Location = New System.Drawing.Point(179, 261)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(66, 61)
+        Me.Label8.TabIndex = 21
+        Me.Label8.Tag = "7"
+        Me.Label8.Text = "00"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label9
+        '
+        Me.Label9.BackColor = System.Drawing.Color.Red
+        Me.Label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label9.ForeColor = System.Drawing.Color.White
+        Me.Label9.Location = New System.Drawing.Point(104, 261)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(66, 61)
+        Me.Label9.TabIndex = 20
+        Me.Label9.Tag = "6"
+        Me.Label9.Text = "00"
+        Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AllowDrop = True
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(57, 344)
+        Me.TextBox1.MaximumSize = New System.Drawing.Size(1000, 1000)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(563, 149)
+        Me.TextBox1.TabIndex = 26
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(724, 537)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.SuperStarCBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.SStarLbl)
+        Me.Controls.Add(Me.Sayi6Lbl)
+        Me.Controls.Add(Me.Sayi5Lbl)
+        Me.Controls.Add(Me.Sayi4Lbl)
+        Me.Controls.Add(Me.Sayi3Lbl)
+        Me.Controls.Add(Me.Sayi2Lbl)
+        Me.Controls.Add(Me.OynaBtn)
+        Me.Controls.Add(Me.Sayi1Lbl)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Tag = ""
+        Me.Text = "SayısalLoto"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SuperStarCBox As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents SStarLbl As Label
+    Friend WithEvents Sayi6Lbl As Label
+    Friend WithEvents Sayi5Lbl As Label
+    Friend WithEvents Sayi4Lbl As Label
+    Friend WithEvents Sayi3Lbl As Label
+    Friend WithEvents Sayi2Lbl As Label
+    Friend WithEvents OynaBtn As Button
+    Friend WithEvents Sayi1Lbl As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ SayısalLoto/SayısalLoto/Form1.vb
@@ -0,0 +1,85 @@
+﻿Public Class Form1
+    Private Sub OynaBtn_Click(sender As Object, e As EventArgs) Handles OynaBtn.Click
+        Dim rnd As New Random
+        Dim sayi, superstarsayi As Integer
+        Dim sayilar(5) As Integer
+        Dim eslesme As Boolean = False
+        Dim mesaj1, mesaj2 As String
+        mesaj1 = "EŞLEŞME VAR,KAZANDINIZ! TEBRİKLER!"
+        mesaj2 = "Eşleşme yok. Kazanamadınız."
+
+        For i = 0 To sayilar.Count - 1
+            Do
+                sayi = rnd.Next(1, 91)
+            Loop While sayilar.Contains(sayi)
+            sayilar(i) = sayi
+        Next
+        Array.Sort(sayilar)
+
+        For Each lbl As Label In Me.Controls.OfType(Of Label)
+            If IsNumeric(lbl.Tag) Then
+                If lbl.Tag >= 0 AndAlso lbl.Tag < 6 Then
+                    lbl.Text = sayilar(lbl.Tag)
+                End If
+            End If
+        Next
+
+        If SuperStarCBox.Checked Then
+            superstarsayi = rnd.Next(1, 91)
+        End If
+        SStarLbl.Text = superstarsayi
+
+        eslesme = eslesmekontrolu()
+        If eslesme = True Then
+            TextBox1.Text = mesaj1
+        Else
+            TextBox1.Text = mesaj2
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Rnd As New Random
+        Dim sayi2 As Integer
+        Dim sayilar2(12) As Integer
+
+        For i = 0 To sayilar2.Count - 1
+            Do
+                sayi2 = Rnd.Next(1, 91)
+            Loop While sayilar2.Contains(sayi2)
+            sayilar2(i) = sayi2
+        Next
+        Array.Sort(sayilar2)
+
+        For Each lbl2 As Label In Me.Controls.OfType(Of Label)
+            If IsNumeric(lbl2.Tag) Then
+                If lbl2.Tag > 5 AndAlso lbl2.Tag < 12 Then
+                    lbl2.Text = sayilar2(lbl2.Tag)
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Function eslesmekontrolu() As Boolean
+        'If Sayi1Lbl.Text = lbl2.text Then
+        '    eslesme = True
+        '    ListView1.Items.Add("KAZANDINIZ! TEBRİKLER!")
+        'End If
+        Dim mesaj1, mesaj2 As String
+        mesaj1 = "KAZANDINIZ! TEBRİKLER!"
+        mesaj2 = "Eşleşme yok. Kazanamadınız."
+        For Each lbl As Label In Me.Controls.OfType(Of Label)
+            If IsNumeric(lbl.Tag) AndAlso (lbl.Tag) >= 0 AndAlso (lbl.Tag) <= 5 Then
+                For Each lbl2 As Label In Me.Controls.OfType(Of Label)
+                    If IsNumeric(lbl2.Tag) AndAlso (lbl2.Tag) >= 6 AndAlso (lbl2.Tag) <= 11 Then
+                        If lbl.Text = lbl2.Text Then
+                            Return True
+                            TextBox1.Text = mesaj1
+                        End If
+                    End If
+                Next
+            End If
+        Next
+        Return False
+        TextBox1.Text = mesaj2
+    End Function
+End Class
